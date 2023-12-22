0b5b8a6a434a73248068c7be0c5e7736d72e0c7b
Laura Garrido Arredondo <anoobmedic@gmail.com>
2023-12-22 15:40:44 +0100

    Entidades con TypeORM y funcionando endpoints

--- src/res/api-funkos/categoria/res/entities/categoria.entity.ts
+++ src/res/api-funkos/categoria/res/entities/categoria.entity.ts
@@ -1,7 +1,17 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+@Entity('categoria')
 export class categoria {
+  @PrimaryGeneratedColumn()
   categoriaid: number
+  @Column('varchar', { length: 255, nullable: false, name: 'Nombre_Categoria' })
   nombre: string
   fecha_creacion: Date
   fecha_actualizacion: Date
+  @Column('varchar', {
+    length: 150,
+    nullable: false,
+    name: 'isActive_Categorias',
+    default: false,
+  })
   isactive: boolean
 }
--- src/res/api-funkos/res/entities/re.entity.ts
+++ src/res/api-funkos/res/entities/re.entity.ts
@@ -1,11 +1,15 @@
 import {CategoriaCreateDTO} from "../../categoria/res/dto/CategoriaCreateDTO";
-
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+@Entity('funkos')
 export class Re {
+  @PrimaryGeneratedColumn()
   id: number
   categoria: CategoriaCreateDTO
+  @Column('varchar', { length: 255, nullable: false, name: 'Nombre' })
   nombre: string
   precio: number
   cantidad: number
+  @Column('varchar', { length: 255, nullable: false, name: 'imagen' })
   imagen: string
   fecha_creacion: Date
   fecha_actualizacion: Date
